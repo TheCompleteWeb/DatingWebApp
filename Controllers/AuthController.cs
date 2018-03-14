449177315e0204b529dd9d02297c94afcad73208
neogeoss <neogeo-s@hanmail.net>
2018-03-14 21:59:37 +0900

    Validation in register methods

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -18,11 +18,15 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
-            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            
+             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if(await _repo.UserExists(userForRegisterDto.Username))
-                return BadRequest("Username is already taken");
+                ModelState.AddModelError("Username", "Username already exists");
 
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+  
             var userToCreate = new User
             {
                 Username = userForRegisterDto.Username
